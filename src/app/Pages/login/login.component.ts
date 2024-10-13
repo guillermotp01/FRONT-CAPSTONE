8056aa0df2237e9eb0d12b356d518dd1f2ee293d
Marfalling <dame.tufuerza.pegazo988@gmail.com>
2024-10-13 00:59:54 -0500

    inicia sesión como administrador y si no tiene cuenta registrada, no inicia

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../auth.service';  // Ruta corregida
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';  // Importar FormsModule
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, HttpClientModule, FormsModule],  // FormsModule agregado
+  providers: [AuthService],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export default class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  private router = inject(Router)
+  dashboard(){
+    this.router.navigate(['dashboard']);
+  }
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        if (response.token) {
+          this.authService.saveToken(response.token); // Guardar el token si es generado
+          console.log('Login exitoso!', response);
+          this.dashboard();
+        } else {
+          console.error('Login fallido', response.Estado);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Fallaste en tu inicio de sesión tontin",
+          });
+        }
+      },
+      error: (err) => {
+        console.error('Error en la solicitud', err);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Error de Servidor",
+        });
+      }
+    });
+  }
 }
